14b18508a82c31b07120793b33f56dd396f45d31
cristianOspina <desarrollo4@aveonline.co>
2021-06-24 19:35:59 -0500

    add types var

---
+++ var-constants/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const pi float64 = 3.14
+	const pi2 =3.14
+
+	base := 12
+
+	var altura int = 14
+	var area int
+
+	var a int // 0
+	var b float64 // 0
+	var c string // vacio
+	var d bool // false
+
+	fmt.Println(pi)
+	fmt.Println((pi2))
+	fmt.Println((altura))
+	fmt.Println((base))
+	fmt.Println((area))
+	fmt.Println(a, b, c, d)
+}
